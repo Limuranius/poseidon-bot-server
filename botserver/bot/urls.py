c73a10314315dc988785ead58ba0f7442762fd6d
Limuran <strelgl2@gmail.com>
2022-12-11 20:34:44 +1000

    Просмотр информации о каждой записи

--- botserver/bot/urls.py
+++ botserver/bot/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import IndexView, CreateView, RecordsListView
+from .views import IndexView, CreateRecordView, RecordsListView, RecordView
 
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index_url"),
-    path("create/", CreateView.as_view(), name="create_record_url"),
-    path("list/", RecordsListView.as_view(), name="records_list_url"),
+    path("create/", CreateRecordView.as_view(), name="create_record_url"),
+    path("records/", RecordsListView.as_view(), name="records_list_url"),
+    path("records/<int:record_id>", RecordView.as_view(), name="record_url")
 ]
--- botserver/bot/views.py
+++ botserver/bot/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import RecordForm
 from .models import RecordModel
 from .BotManagment import BotMonitor
@@ -10,22 +11,30 @@ class IndexView(View):
         return render(request, "bot/index.html")
 
 
-class CreateView(View):
+class CreateRecordView(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, "bot/create_record.html", context={"form": RecordForm()})
 
     def post(self, request):
         form = RecordForm(request.POST)
         if form.is_valid():
-            obj = form.save()  # Сохраняем новую запись в БД
+            # Сохраняем новую запись в БД
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
             BotMonitor.update_bot_for_obj(obj)  # Создаём для записи связанного бота
             return redirect("records_list_url")
         else:
             return redirect("create_record_url")
 
 
-
 class RecordsListView(View):
     def get(self, request):
         records = RecordModel.objects.all()
         return render(request, "bot/record_list.html", context={"records": records})
+
+
+class RecordView(View):
+    def get(self, request, record_id: int):
+        record = get_object_or_404(RecordModel, pk=record_id)
+        return render(request, "bot/record_info.html", context={"record": record})
