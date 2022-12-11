70ebeda7d84cb75c145773b713044d77bc436ad7
Limuran <strelgl2@gmail.com>
2022-12-11 20:39:00 +1000

    Небольшие эстетические исправления

--- botserver/bot/forms.py
+++ botserver/bot/forms.py
@@ -5,29 +5,26 @@ from .models import RecordModel
 class RecordForm(forms.ModelForm):
     class Meta:
         model = RecordModel
-        fields = ["open_time", "date", "min_duration",
+        fields = ["open_datetime", "date", "min_duration",
                   "max_duration", "machine_number", "time_from",
-                  "time_before", "login", "password", "user_name"]
+                  "time_before", "user_name"]
 
         widgets = {
-            "open_time": forms.TimeInput(attrs={"type": "time"}),
+            "open_datetime": forms.DateTimeInput(attrs={"type": "datetime-local"}),
             "date": forms.DateInput(attrs={"type": "date"}),
             "min_duration": forms.DateInput(attrs={"type": "time", "step": 900}),
             "max_duration": forms.DateInput(attrs={"type": "time", "step": 900}),
             "time_from": forms.DateInput(attrs={"type": "time", "step": 900}),
             "time_before": forms.DateInput(attrs={"type": "time", "step": 900}),
-            "password": forms.DateInput(attrs={"type": "password"}),
         }
 
         labels = {
-            "open_time": "Запись открывается в",
+            "open_datetime": "Запись открывается в",
             "date": "Дата",
             "min_duration": "Минимальная длительность",
             "max_duration": "Максимальная длительность",
             "machine_number": "Номер машинки",
             "time_from": "Искать от",
             "time_before": "Искать до",
-            "login": "Логин от ДВФУ",
-            "password": "Пароль от ДВФУ",
             "user_name": "Имя",
         }
--- botserver/bot/poseidonbot/DjangoExtensions/ConfigManagerDB.py
+++ botserver/bot/poseidonbot/DjangoExtensions/ConfigManagerDB.py
@@ -1,27 +1,24 @@
 from ..ConfigManager import ConfigManagerInterface
 from django.db.models import Model
-from typing import Type
 
 
 class ConfigManagerDB(ConfigManagerInterface):
-    _model: Type[Model]
-    _related_config_id: int
+    __model_obj: Model
 
-    def __init__(self, model: Type[Model], related_config_id: int):
+    def __init__(self, obj: Model):
         super().__init__()
-        self._model = model
-        self._related_config_id = related_config_id
+        self.__model_obj = obj
         self.load()
 
     def load(self) -> None:
-        obj = self._model.objects.get(pk=self._related_config_id)
-        self._config.ExecuteAt = obj.open_time
-        self._config.Date = obj.date
-        self._config.MinTimeLength = obj.min_duration
-        self._config.MaxTimeLength = obj.max_duration
-        self._config.MachineNumber = obj.machine_number
-        self._config.TimeIntervals = [(obj.time_from, obj.time_before)]
-        self._user_data.username = obj.login
-        self._user_data.password = obj.password
+        self.__model_obj.refresh_from_db()  # Синхронизировать объект с БД
+        self._config.ExecuteAt = self.__model_obj.open_datetime
+        self._config.Date = self.__model_obj.date
+        self._config.MinTimeLength = self.__model_obj.min_duration
+        self._config.MaxTimeLength = self.__model_obj.max_duration
+        self._config.MachineNumber = self.__model_obj.machine_number
+        self._config.TimeIntervals = [(self.__model_obj.time_from, self.__model_obj.time_before)]
+        self._user_data.username = self.__model_obj.owner.FEFU_username
+        self._user_data.password = self.__model_obj.owner.FEFU_password
         self._config.POST_COUNT_BEFORE_UPDATE = 3
         self._config.LOG_SCHEDULES = False
