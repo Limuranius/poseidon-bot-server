93f864f3e7efb16d4ca088a03b8e253cc71fbaf9
Limuran <strelgl2@gmail.com>
2022-12-13 06:50:02 +1000

    Улучшение форм

--- botserver/users/forms.py
+++ botserver/users/forms.py
@@ -1,11 +1,23 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = ("username", "password1", "password2", "FEFU_username", "FEFU_password")
+        widgets = {
+            "FEFU_password": forms.PasswordInput()
+        }
+
+        labels = {
+            "username": "Логин",
+            "password1": "Пароль",
+            "password2": "Подтверждение пароля",
+            "FEFU_username": "Логин от сайта ДВФУ",
+            "FEFU_password": "Пароль от сайта ДВФУ",
+        }
 
 
 class CustomUserChangeForm(UserChangeForm):
